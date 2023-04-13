7efafc053e403b2b0e2c02c7ccecd895b4cd91fb
Haeseong Moon <hsmoonjohn@gmail.com>
2023-04-12 20:08:20 -0700

    fit_nl

--- WAQR.py
+++ WAQR.py
@@ -9,8 +9,8 @@ import torch.optim as optim
 # import time
 from DeepQuantile import DQR
 from sklearn.linear_model import LinearRegression
-
-
+from torch.utils.data import DataLoader, TensorDataset
+from torch.optim.lr_scheduler import StepLR
 
 class WAQR:
     '''
@@ -67,7 +67,8 @@ class WAQR:
         r1 = self.pseudo_outcome(qy1=qy1, tau1=tau1, weighting=weighting)
         return LinearRegression(fit_intercept=intercept).fit(self.X, r1)
 
-    def fit_nl(self, tau1, weighting ='es', hidden_sizes=[128,128], num_epochs=200, loss_function = 'MSE'):
+    def fit_nl(self, tau1, weighting ='es', hidden_sizes=[128, 128], num_epochs=200, loss_function='MSE', batch_size=64,
+               use_lr_decay=False, dropout_rate=0.1):
         qnet1 = DQR(self.X, self.Y, options=self.opt)
         if weighting == 'es':
             qnet1.fit(tau=tau1)
@@ -75,7 +76,8 @@ class WAQR:
             qnet1.fit(tau=1-tau1)
         qy1 = qnet1.predict(self.X)
         r1 = self.pseudo_outcome(qy1=qy1, tau1=tau1, weighting=weighting)
-        model=FullyConnectedNN(input_size=self.X.shape[1], output_size=1, hidden_sizes=hidden_sizes)
+        model = FullyConnectedNN(input_size=self.X.shape[1], output_size=1, hidden_sizes=hidden_sizes,
+                               dropout_rate=dropout_rate)
         if loss_function == 'MSE':
             criterion = nn.MSELoss()
         elif loss_function == 'MAE':
@@ -85,34 +87,53 @@ class WAQR:
         else:
             raise ValueError("Invalid loss function")
 
-        optimizer = optim.Adam(model.parameters(),lr=self.opt['lr'])
-        for epoch in range(num_epochs):
+        train_x = torch.tensor(self.X, dtype=torch.float32).view(-1, 1)
+        train_y = torch.tensor(r1, dtype=torch.float32).view(-1, 1)
+        train_data = TensorDataset(train_x, train_y)
+        train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+        epoch = 0
+        loss_diff = 1
+        optimizer = optim.Adam(model.parameters(), lr=self.opt['lr'])
+        if use_lr_decay:
+            scheduler = StepLR(optimizer, step_size=100, gamma=0.1)
+        while epoch < num_epochs and loss_diff > self.opt['tol']:
+            train_loss = torch.Tensor([0])
             model.train()
-            optimizer.zero_grad()
-            predictions = model(train_x)
-            loss = criterion(predictions, train_y)
-            loss.backward()
-            optimizer.step()
+            for batch_x, batch_y in train_loader:
+                optimizer.zero_grad()
+                predictions = model(batch_x)
+                loss = criterion(predictions, batch_y)
+                loss.backward()
+                optimizer.step()
+                train_loss += loss.data
+            if epoch != 0:
+                loss_diff = np.abs(train_loss.data.numpy() / float(len(train_loader))-prev)
+            prev = train_loss.data.numpy() / float(len(train_loader))
+            if use_lr_decay:
+                scheduler.step()
+            epoch += 1
 
-
+        return model
 
 
 
 class FullyConnectedNN(nn.Module):
-    def __init__(self, input_size, output_size, hidden_sizes):
+    def __init__(self, input_size, output_size, hidden_sizes, dropout_rate):
         super(FullyConnectedNN, self).__init__()
         self.layers = nn.ModuleList()
         layer_sizes = [input_size] + hidden_sizes + [output_size]
 
         for i in range(len(layer_sizes) - 1):
             self.layers.append(nn.Linear(layer_sizes[i], layer_sizes[i + 1]))
+            if i < len(layer_sizes) - 2:
+                self.layers.append(nn.ReLU())
+                self.layers.append(nn.Dropout(dropout_rate))
 
     def forward(self, x):
-        for i, layer in enumerate(self.layers):
+        for layer in self.layers:
             x = layer(x)
-            if i < len(self.layers) - 1:
-                x = torch.relu(x)
         return x
+
 
 
 
